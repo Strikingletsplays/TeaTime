192a50a261fbe147562ac4ebf5651fe158cfb192
Alesence <78483468+Alesence@users.noreply.github.com>
2024-01-27 23:49:03 +0200

    AI + NavMesh Implemented

--- Assets/Script/EnemyAi.cs
+++ Assets/Script/EnemyAi.cs
@@ -22,6 +22,7 @@ public class EnemyAi : MonoBehaviour
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        Debug.Log("YO WTF");
     }
 
     private void Update()
